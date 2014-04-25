253c2445ca8070e720e1e86b3063adc923977641
Steven Deobald <steven@nilenso.com>
2014-04-25 20:23:49 +0530

    Initial commit! Includes sample test.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject protoclj "0.1.0-SNAPSHOT"
+  :description "A protobuffer compiler for clojure wrappers"
+  :url "https://github.com/nilenso/protoclj"
+  :license {:name "The MIT License"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
